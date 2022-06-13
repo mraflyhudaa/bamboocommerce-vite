c389d314ae4e1ad3f1e5a568cb9e91c60d61f210
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-13 21:29:48 +0700

    change mobile menu z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -116,7 +116,7 @@ const Navbar = () => {
     <div className='sticky top-0 z-50 bg-white'>
       {/* Mobile menu */}
       <Transition.Root show={open} as={Fragment}>
-        <Dialog as='div' className='relative z-40 lg:hidden' onClose={setOpen}>
+        <Dialog as='div' className='relative z-50 lg:hidden' onClose={setOpen}>
           <Transition.Child
             as={Fragment}
             enter='transition-opacity ease-linear duration-300'
