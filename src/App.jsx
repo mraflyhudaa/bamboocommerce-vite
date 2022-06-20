9aacfb3f2ec3d0a7e67a659603152c043a071963
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-20 13:54:11 +0700

    integrate react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
+import Checkout from './pages/Checkout';
 import Home from './pages/Home';
-import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
+import ProductList from './pages/ProductList';
 import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const user = true;
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/products' element={<ProductList />} />
-      <Route path='/products/1' element={<ProductDetail />} />
-      <Route path='/signin' element={<Signin />} />
-    </Routes>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/products/:category'>
+          <ProductList />
+        </Route>
+        <Route path='/product/:id'>
+          <ProductDetail />
+        </Route>
+        <Route path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='/signin'>
+          {user ? <Redirect to={'/'} /> : <Signin />}
+        </Route>
+        <Route path='/signup'>
+          {user ? <Redirect to={'/'} /> : <Signup />}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
