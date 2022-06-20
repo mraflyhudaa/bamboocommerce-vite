60d9446c90532128c9ce4e53a6b9b1f234a61aad
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-20 13:53:22 +0700

    some changes to dummy data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,69 @@
+export const navigation = {
+  categories: [
+    {
+      id: 'products',
+      name: 'Products',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '/products',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+          imageAlt:
+            'Models sitting back to back, wearing Basic Tee in black and bone.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc:
+            'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+          imageAlt:
+            'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+        },
+      ],
+      sections: [
+        {
+          id: 'outdoors',
+          name: 'Outdoors',
+          items: [
+            { name: 'Bamboo Fences', href: '#' },
+            { name: 'Bamboo Gate Doors', href: '#' },
+            { name: 'Bamboo Mats', href: '#' },
+            { name: 'Bamboo Poles', href: '#' },
+            { name: 'Bamboo Poles Half Round', href: '#' },
+            { name: 'Bamboo Pergolas', href: '#' },
+            { name: 'Bamboo Borders', href: '#' },
+            { name: 'Bamboo Slats', href: '#' },
+          ],
+        },
+        {
+          id: 'indoors',
+          name: 'Indoors',
+          items: [
+            { name: 'Bamboo & Reed Roller Blinds', href: '#' },
+            { name: 'Bamboo Room Dividers', href: '#' },
+          ],
+        },
+        // {
+        //   id: 'building',
+        //   name: 'Building',
+        //   items: [
+        //     { name: 'Full Nelson', href: '#' },
+        //     { name: 'My Way', href: '#' },
+        //     { name: 'Re-Arranged', href: '#' },
+        //     { name: 'Counterfeit', href: '#' },
+        //     { name: 'Significant Other', href: '#' },
+        //   ],
+        // },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+};
+
 export const products = [
   {
     id: 1,
@@ -91,7 +157,7 @@ export const sortOptions = [
 ];
 
 export const subCategories = [
-  { name: 'Totes', href: '#' },
+  { name: 'Bamboo Poles', href: '#' },
   { name: 'Backpacks', href: '#' },
   { name: 'Travel Bags', href: '#' },
   { name: 'Hip Bags', href: '#' },
@@ -100,38 +166,92 @@ export const subCategories = [
 
 export const filters = [
   {
-    id: 'color',
-    name: 'Color',
+    id: 'outside',
+    name: 'Outside',
     options: [
-      { value: 'white', label: 'White', checked: false },
-      { value: 'beige', label: 'Beige', checked: false },
-      { value: 'blue', label: 'Blue', checked: true },
-      { value: 'brown', label: 'Brown', checked: false },
-      { value: 'green', label: 'Green', checked: false },
-      { value: 'purple', label: 'Purple', checked: false },
+      {
+        value: 'outside',
+        label: 'All Products',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo fence',
+        label: 'Bamboo Fence',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo gate door',
+        label: 'Bamboo Gate Door',
+        checked: false,
+        href: '#',
+      },
+      { value: 'bamboo mats', label: 'Bamboo Mats', checked: true, href: '#' },
+      {
+        value: 'bamboo poles',
+        label: 'Bamboo Poles',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo poles half round',
+        label: 'Bamboo Poles Half Round',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo pergolas',
+        label: 'Bamboo Pergolas',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo borders',
+        label: 'Bamboo Borders',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo slats',
+        label: 'Bamboo Slats',
+        checked: false,
+        href: '#',
+      },
     ],
   },
   {
-    id: 'category',
-    name: 'Category',
+    id: 'inside',
+    name: 'Inside',
     options: [
-      { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-      { value: 'sale', label: 'Sale', checked: false },
-      { value: 'travel', label: 'Travel', checked: true },
-      { value: 'organization', label: 'Organization', checked: false },
-      { value: 'accessories', label: 'Accessories', checked: false },
+      {
+        value: 'bamboo and red roller blinds',
+        label: 'Bamboo & Reed Roller Blinds',
+        checked: false,
+        href: '#',
+      },
+      {
+        value: 'bamboo room dividers',
+        label: 'Bamboo Room Dividers',
+        checked: false,
+        href: '#',
+      },
     ],
   },
-  {
-    id: 'size',
-    name: 'Size',
-    options: [
-      { value: '2l', label: '2L', checked: false },
-      { value: '6l', label: '6L', checked: false },
-      { value: '12l', label: '12L', checked: false },
-      { value: '18l', label: '18L', checked: false },
-      { value: '20l', label: '20L', checked: false },
-      { value: '40l', label: '40L', checked: true },
-    ],
-  },
+  // {
+  //   id: 'category',
+  //   name: 'Category',
+  //   options: [
+  //     { value: 'outside', label: 'New Arrivals', checked: false, href: '#' },
+  //     { value: 'sale', label: 'Sale', checked: false, href: '#' },
+  //     { value: 'travel', label: 'Travel', checked: true, href: '#' },
+  //     {
+  //       value: 'organization',
+  //       label: 'Organization',
+  //       checked: false,
+  //       href: '#',
+  //     },
+  //     { value: 'accessories', label: 'Accessories', checked: false, href: '#' },
+  //   ],
+  // },
 ];
