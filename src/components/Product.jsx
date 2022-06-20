29852da421f5dbde2ddef038ed7e46311c9afd44
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-20 16:15:07 +0700

    add condition to render products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 // import { products } from '../data';
 
 const Product = (props) => {
@@ -14,7 +15,7 @@ const Product = (props) => {
         const res = await axios.get(
           cat
             ? `http://167.172.72.229:3000/api/products?category=${cat}`
-            : 'http://167.172.72.229:3000/api/products'
+            : 'http://167.172.72.229:3000/api/products/'
         );
         setProducts(res.data.data);
       } catch (error) {
@@ -50,25 +51,47 @@ const Product = (props) => {
       );
     }
   }, [sort]);
-  console.log(sort);
+  console.log(products);
 
   return (
     <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
-      {filteredProducts.map((product) => (
-        <a key={product.id} href={product.href} className='group'>
-          <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
-            <img
-              src={product.imageSrc}
-              alt={product.imageAlt}
-              className='w-full h-full object-center object-cover group-hover:opacity-75 group-hover:scale-110 transition-all'
-            />
-          </div>
-          <h3 className='mt-4 text-sm text-gray-700'>{product.title}</h3>
-          <p className='mt-1 text-lg font-medium text-gray-900'>
-            {product.price[0]}
-          </p>
-        </a>
-      ))}
+      {cat
+        ? filteredProducts.map((product) => (
+            <Link
+              key={product._id}
+              to={`product/${product._id}`}
+              className='group'>
+              <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className='w-full h-full object-center object-cover group-hover:opacity-75 group-hover:scale-110 transition-all'
+                />
+              </div>
+              <h3 className='mt-4 text-sm text-gray-700'>{product.title}</h3>
+              <p className='mt-1 text-lg font-medium text-gray-900'>
+                {product.price[0]}
+              </p>
+            </Link>
+          ))
+        : products.map((product) => (
+            <Link
+              key={product._id}
+              to={`product/${product._id}`}
+              className='group'>
+              <div className='w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8'>
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className='w-full h-full object-center object-cover group-hover:opacity-75 group-hover:scale-110 transition-all'
+                />
+              </div>
+              <h3 className='mt-4 text-sm text-gray-700'>{product.title}</h3>
+              <p className='mt-1 text-lg font-medium text-gray-900'>
+                {product.price[0]}
+              </p>
+            </Link>
+          ))}
     </div>
   );
 };
