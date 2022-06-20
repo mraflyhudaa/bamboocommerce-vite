6e4b7b9e4eb28e919735e3bf70cc36f16b328b4e
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-20 13:50:30 +0700

    Delete breadcrumb component

--- src/components/Breadcrumb.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Breadcrumbs as MUIBreadcrumbs, Link, Typography } from '@mui/material';
-
-
-const Breadcrumb = () => {
-  return <div>Breadcrumb</div>;
-};
-
-export default Breadcrumb;
