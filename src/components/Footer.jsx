d3115957bd9070fb6b445425304ff22633513ffa
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-20 16:14:51 +0700

    rename class attribute to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,19 +8,19 @@ import {
 
 const Footer = () => {
   return (
-    <footer class='text-center lg:text-left bg-gray-100 text-gray-600'>
-      <div class='flex justify-center items-center lg:justify-between p-6 border-b border-gray-300'>
-        <div class='mr-12 hidden lg:block'>
+    <footer className='text-center lg:text-left bg-gray-100 text-gray-600'>
+      <div className='flex justify-center items-center lg:justify-between p-6 border-b border-gray-300'>
+        <div className='mr-12 hidden lg:block'>
           <span>Get connected with us on social networks:</span>
         </div>
-        <div class='flex justify-center'>
-          <a href='#!' class='mr-6 text-gray-600'>
+        <div className='flex justify-center'>
+          <a href='#!' className='mr-6 text-gray-600'>
             <svg
               aria-hidden='true'
               focusable='false'
               data-prefix='fab'
               data-icon='facebook-f'
-              class='w-2.5'
+              className='w-2.5'
               role='img'
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 320 512'>
@@ -29,13 +29,13 @@ const Footer = () => {
                 d='M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z'></path>
             </svg>
           </a>
-          <a href='#!' class='mr-6 text-gray-600'>
+          <a href='#!' className='mr-6 text-gray-600'>
             <svg
               aria-hidden='true'
               focusable='false'
               data-prefix='fab'
               data-icon='twitter'
-              class='w-4'
+              className='w-4'
               role='img'
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 512 512'>
@@ -44,13 +44,15 @@ const Footer = () => {
                 d='M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z'></path>
             </svg>
           </a>
-          <a href='http://instagram.com/bamboo.ina' class='mr-6 text-gray-600'>
+          <a
+            href='http://instagram.com/bamboo.ina'
+            className='mr-6 text-gray-600'>
             <svg
               aria-hidden='true'
               focusable='false'
               data-prefix='fab'
               data-icon='instagram'
-              class='w-3.5'
+              className='w-3.5'
               role='img'
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 448 512'>
@@ -61,11 +63,11 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <div class='mx-6 py-10 text-center md:text-left'>
-        <div class='grid grid-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-          <div class=''>
+      <div className='mx-6 py-10 text-center md:text-left'>
+        <div className='grid grid-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          <div className=''>
             <h6
-              class='
+              className='
             uppercase
             font-semibold
             mb-4
@@ -79,7 +81,7 @@ const Footer = () => {
                 focusable='false'
                 data-prefix='fas'
                 data-icon='cubes'
-                class='w-4 mr-3'
+                className='w-4 mr-3'
                 role='img'
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 512 512'>
@@ -94,47 +96,47 @@ const Footer = () => {
               craft. Export to worldwide.
             </p>
           </div>
-          <div class=''>
-            <h6 class='uppercase font-semibold mb-4 flex justify-center md:justify-start'>
+          <div className=''>
+            <h6 className='uppercase font-semibold mb-4 flex justify-center md:justify-start'>
               Products
             </h6>
-            <p class='mb-4'>
-              <a href='#!' class='text-gray-600'>
+            <p className='mb-4'>
+              <a href='#!' className='text-gray-600'>
                 Outside
               </a>
             </p>
-            <p class='mb-4'>
-              <a href='#!' class='text-gray-600'>
+            <p className='mb-4'>
+              <a href='#!' className='text-gray-600'>
                 Inside
               </a>
             </p>
-            <p class='mb-4'>
-              <a href='#!' class='text-gray-600'>
+            <p className='mb-4'>
+              <a href='#!' className='text-gray-600'>
                 Building
               </a>
             </p>
           </div>
-          <div class=''>
-            <h6 class='uppercase font-semibold mb-4 flex justify-center md:justify-start'>
+          <div className=''>
+            <h6 className='uppercase font-semibold mb-4 flex justify-center md:justify-start'>
               Contact
             </h6>
-            <p class='flex items-center justify-center md:justify-start mb-4'>
+            <p className='flex items-center justify-center md:justify-start mb-4'>
               <HomeIcon className='w-4 mr-4' />
               Yogyakarta, Indonesia
             </p>
-            <p class='flex items-center justify-center md:justify-start mb-4'>
+            <p className='flex items-center justify-center md:justify-start mb-4'>
               <MailIcon className='w-4 mr-4' />
               dekorindojayamandiri@gmail.com
             </p>
-            <p class='flex items-center justify-center md:justify-start mb-4'>
+            <p className='flex items-center justify-center md:justify-start mb-4'>
               <PhoneIcon className='w-4 mr-4' />+ 01 234 567 88
             </p>
           </div>
         </div>
       </div>
-      <div class='text-center p-6 bg-gray-200'>
+      <div className='text-center p-6 bg-gray-200'>
         <span>© 2022 Copyright </span>
-        <a class='text-gray-600 font-semibold' href='#!'>
+        <a className='text-gray-600 font-semibold' href='#!'>
           Dekorindo Jaya Mandiri
         </a>
       </div>
