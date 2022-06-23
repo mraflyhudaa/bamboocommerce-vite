882c678242c6cc7ae1943836bf9fe54f4b258ed7
mraflyhudaa <mraflyhudaa@gmail.com>
2022-06-23 19:42:04 +0700

    integrate midtrans script

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,9 +14,29 @@ import ProductDetail from './pages/ProductDetail';
 import ProductList from './pages/ProductList';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Success from './pages/Success';
 
 const App = () => {
-  const user = true;
+  const quantity = useSelector((state) => state.cart.quantity);
+  const user = false;
+
+  useEffect(() => {
+    //change this to the script source you want to load, for example this is snap.js sandbox env
+    const midtransScriptUrl = 'https://app.sandbox.midtrans.com/snap/snap.js';
+    //change this according to your client-key
+    const myMidtransClientKey = import.meta.env.MIDTRANS_CLIENT_KEY;
+
+    let scriptTag = document.createElement('script');
+    scriptTag.src = midtransScriptUrl;
+    // optional if you want to set script attribute
+    // for example snap.js have data-client-key attribute
+    scriptTag.setAttribute('data-client-key', myMidtransClientKey);
+
+    document.body.appendChild(scriptTag);
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
 
   return (
     <Router>
@@ -24,6 +45,9 @@ const App = () => {
         <Route exact path='/'>
           <Home />
         </Route>
+        <Route path='/products'>
+          <ProductList />
+        </Route>
         <Route path='/products/:category'>
           <ProductList />
         </Route>
@@ -31,7 +55,10 @@ const App = () => {
           <ProductDetail />
         </Route>
         <Route path='/checkout'>
-          <Checkout />
+          {quantity === 0 ? <Redirect to={'/'} /> : <Checkout />}
+        </Route>
+        <Route path='/success'>
+          <Success />
         </Route>
         <Route path='/signin'>
           {user ? <Redirect to={'/'} /> : <Signin />}
