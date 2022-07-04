66834ff0d325d2f645565d571dd41e0590533349
mraflyhudaa <mraflyhudaa@gmail.com>
2022-07-04 22:16:41 +0700

    add new state properties

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,12 +1,33 @@
-import { loginFailure, loginStart, loginSuccess } from './userRedux';
+import {
+  loginFailure,
+  loginStart,
+  loginSuccess,
+  registerFailure,
+  registerStart,
+  registerSuccess,
+} from './userRedux';
 import { publicRequest } from '../requestMethods';
 
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
-  try {
-    const res = await publicRequest.post('/auth/login', user);
-    dispatch(loginSuccess(res.data));
-  } catch (err) {
-    dispatch(loginFailure(err));
-  }
+  await publicRequest
+    .post('/auth/login', user)
+    .then((res) => {
+      dispatch(loginSuccess(res.data.data));
+    })
+    .catch((err) => {
+      dispatch(loginFailure(err.response.data.message));
+    });
+};
+
+export const register = async (dispatch, user) => {
+  dispatch(registerStart());
+  await publicRequest
+    .post('/auth/register', user)
+    .then((res) => {
+      dispatch(registerSuccess(res.data.message));
+    })
+    .catch((err) => {
+      dispatch(registerFailure(err.response.data.message));
+    });
 };
--- src/redux/userRedux.js
+++ src/redux/userRedux.js
@@ -5,7 +5,9 @@ const userSlice = createSlice({
   initialState: {
     currentUser: null,
     isFetching: false,
+    isSuccess: false,
     error: false,
+    message: '',
   },
   reducers: {
     loginStart: (state) => {
@@ -13,14 +15,49 @@ const userSlice = createSlice({
     },
     loginSuccess: (state, action) => {
       state.isFetching = false;
+      state.isSuccess = true;
       state.currentUser = action.payload;
     },
-    loginFailure: (state) => {
+    loginFailure: (state, action) => {
       state.isFetching = false;
+      state.isSuccess = false;
       state.error = true;
+      state.message = action.payload;
+    },
+    logout: (state) => {
+      state.currentUser = null;
+    },
+    registerStart: (state) => {
+      state.isFetching = true;
+    },
+    registerSuccess: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = true;
+      state.message = action.payload;
+    },
+    registerFailure: (state, action) => {
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.error = true;
+      state.message = action.payload;
+    },
+    reset: (state) => {
+      state.isFetching = false;
+      state.isSuccess = false;
+      state.error = false;
+      state.message = '';
     },
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const {
+  loginStart,
+  loginSuccess,
+  loginFailure,
+  logout,
+  registerFailure,
+  registerStart,
+  registerSuccess,
+  reset,
+} = userSlice.actions;
 export default userSlice.reducer;
