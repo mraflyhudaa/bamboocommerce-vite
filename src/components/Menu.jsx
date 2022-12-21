02dc4356485312c49ee7db5651a7ecb1817607ea
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2022-12-21 23:05:54 +0700

    add Link to orders page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,16 @@
-import { Menu, Transition } from '@headlessui/react';
-import { Fragment, useEffect, useRef, useState } from 'react';
-import { ChevronDownIcon, LogoutIcon, UserIcon } from '@heroicons/react/solid';
-import { clearCart } from '../redux/cartRedux';
-import { logout } from '../redux/userRedux';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { MdSell } from "react-icons/md";
+import {
+  ChevronDownIcon,
+  LogoutIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
+import { clearCart } from "../redux/cartRedux";
+import { logout } from "../redux/userRedux";
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 
 const MenuDropdown = () => {
   const dispatch = useDispatch();
@@ -17,49 +23,66 @@ const MenuDropdown = () => {
     history.go(0);
   };
   return (
-    <div className='text-right'>
-      <Menu as='div' className='relative inline-block text-left'>
+    <div className="text-right">
+      <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className='inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75'>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md  px-4 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <UserIcon
-              className='ml-2 -mr-4 h-5 w-5 text-gray-400 hover:text-gray-500'
-              aria-hidden='true'
+              className="ml-2 -mr-4 h-5 w-5 text-gray-400 hover:text-gray-500"
+              aria-hidden="true"
             />
           </Menu.Button>
         </div>
         <Transition
           as={Fragment}
-          enter='transition ease-out duration-100'
-          enterFrom='transform opacity-0 scale-95'
-          enterTo='transform opacity-100 scale-100'
-          leave='transition ease-in duration-75'
-          leaveFrom='transform opacity-100 scale-100'
-          leaveTo='transform opacity-0 scale-95'
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
             {/* <div className='px-1 py-1 '>
               <Menu.Item>
                 <span>{user.email}</span>
               </Menu.Item>
             </div> */}
-            <div className='px-1 py-1 '>
+            <div className="px-1 py-1 ">
               <Menu.Item>
-                <span className='group flex w-full justify-center text-gray-900 rounded-md px-2 py-2 text-sm'>
+                <span className="group flex w-full justify-center text-gray-900 rounded-md px-2 py-2 text-sm">
                   {user.email}
                 </span>
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={"/orders"}
+                    className={`${
+                      active ? "bg-green-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    {active ? (
+                      <MdSell className="mr-2 h-5 w-5" aria-hidden="true" />
+                    ) : (
+                      <MdSell className="mr-2 h-5 w-5" aria-hidden="true" />
+                    )}
+                    Orders
+                  </Link>
+                )}
               </Menu.Item>
               <Menu.Item>
                 {({ active }) => (
                   <button
                     onClick={handleClick}
                     className={`${
-                      active ? 'bg-green-500 text-white' : 'text-gray-900'
+                      active ? "bg-green-500 text-white" : "text-gray-900"
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
                     {active ? (
-                      <LogoutIcon className='mr-2 h-5 w-5' aria-hidden='true' />
+                      <LogoutIcon className="mr-2 h-5 w-5" aria-hidden="true" />
                     ) : (
-                      <LogoutIcon className='mr-2 h-5 w-5' aria-hidden='true' />
+                      <LogoutIcon className="mr-2 h-5 w-5" aria-hidden="true" />
                     )}
                     Logout
                   </button>
