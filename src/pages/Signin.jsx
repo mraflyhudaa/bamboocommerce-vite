74f4fe5ea219f6d456c8d3eb8718632350ac41dc
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-02-08 19:49:43 +0700

    fix signin and signup routes

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -72,12 +72,22 @@ const Signin = () => {
               <div className='h-16 flex items-center mx-4 sm:mx-6 lg:mx-8 drop-shadow-xl'>
                 <div className='flex lg:ml-0'>
                   <Link to='/'>
-                    <span className='sr-only'>Workflow</span>
-                    <img
-                      className='h-8 w-auto'
-                      src='https://tailwindui.com/img/logos/workflow-mark.svg?color=green&shade=600'
-                      alt=''
-                    />
+                    <span className='sr-only'>Bamboo</span>
+                    <svg
+                      aria-hidden='true'
+                      focusable='false'
+                      data-prefix='fas'
+                      data-icon='cubes'
+                      className='w-auto h-8 text-green-600 '
+                      role='img'
+                      xmlns='http://www.w3.org/2000/svg'
+                      viewBox='0 0 512 512'
+                    >
+                      <path
+                        fill='currentColor'
+                        d='M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z'
+                      ></path>
+                    </svg>
                   </Link>
                 </div>
                 <div className='ml-auto flex items-center'>
@@ -146,7 +156,8 @@ const Signin = () => {
                 />
                 <label
                   htmlFor='remember-me'
-                  className='ml-2 block text-sm text-gray-900'>
+                  className='ml-2 block text-sm text-gray-900'
+                >
                   Remember me
                 </label>
               </div>
@@ -154,7 +165,8 @@ const Signin = () => {
               <div className='text-sm'>
                 <a
                   href='#'
-                  className='font-medium text-green-600 hover:text-green-500'>
+                  className='font-medium text-green-600 hover:text-green-500'
+                >
                   Forgot your password?
                 </a>
               </div>
@@ -163,7 +175,8 @@ const Signin = () => {
             <div>
               <button
                 type='submit'
-                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'>
+                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
+              >
                 <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                   <LockClosedIcon
                     className='h-5 w-5 text-green-500 group-hover:text-green-400'
